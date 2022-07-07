4447ac90bf0b96c4fba4a0b6bf4efe0ce02fc203
Hristo Atanasov <hristo_atanasov87@abv.bg>
2022-07-07 16:39:17 +0300

    Add files via upload

---
+++ views/allShips.js
@@ -0,0 +1,29 @@
+import { html } from "https://unpkg.com/lit-html?module";
+
+import { getParlours } from "../src/api/data.js";
+
+const allShipsTemplate = (data) => html` <section class="my-cars">
+  <h1>All Cars</h1>
+  <div class="my-collection">
+    ${data.length
+      ? html`${data.map(shipTemplate)}`
+      : html`<p class="no-cars">No shipss in database.</p>`}
+  </div>
+</section>`;
+
+const shipTemplate = (ship) => html`
+  <div>
+    <p>${ship.shipName}</p>
+    <p>${ship.owner}</p>
+    <!-- <img alt="no-pic" src=${ship.img}> -->
+  </div>
+  <div>
+    <a class="btn" href="/edit/${ship.objectId}">Edit</a>
+  </div>
+`;
+
+export async function allShipsPage(ctx) {
+  const [data] = await getShips();
+
+  ctx.render(allShipsTemplate(data));
+}
--- views/register.js
+++ views/register.js
@@ -1,56 +1,57 @@
-import { html } from 'https://unpkg.com/lit-html?module';
+import { html } from "https://unpkg.com/lit-html?module";
 
-import { register } from '../src/api/data.js';
+import { register } from "../src/api/data.js";
 
-const registerTemplate = (onSubmit, isFilled, isMatching) => html`
-<section class="login">
-    <div>
-        <h1>Register</h1>
-        ${isFilled ? html`<p style="color: red">Please fill in all fields!</p>` : ''}
-        ${isMatching ? html`<p style="color: red">Passwords don't match!</p>` : ''}
+const registerTemplate = (onSubmit, isFilled, isMatching) => html` <section
+  class="login"
+>
+  <div>
+    <h1>Register</h1>
+    ${isFilled
+      ? html`<p style="color: red">Please fill in all fields!</p>`
+      : ""}
+    ${isMatching ? html`<p style="color: red">Passwords don't match!</p>` : ""}
+  </div>
+
+  <form @submit=${onSubmit}>
+    <div class="formDiv">
+      <label for="username">Username</label>
+      <input name="username" type="text" />
+      <label for="email">Email</label>
+      <input name="email" type="text" />
+      <label for="password">Password</label>
+      <input name="password" type="password" />
+      <label for="repass">Repeat Password</label>
+      <input name="repass" type="password" />
+
+      <input class="lrBtn" type="submit" value="Register" />
     </div>
-
-    <form @submit=${onSubmit}>
-        <div class="formDiv">
-            <label for="username">Username</label>
-            <input name="username" type="text">
-            <label for="email">Email</label>
-            <input name="email" type="text">
-            <label for="password">Password</label>
-            <input name="password" type="password">
-            <label for="repass">Repeat Password</label>
-            <input name="repass" type="password">
-
-            <input class="lrBtn" type="submit" value="Register">
-        </div>
-    </form>
+  </form>
 </section>`;
 
 export async function registerPage(ctx) {
-    ctx.render(registerTemplate(onSubmit));
+  ctx.render(registerTemplate(onSubmit));
 
-    async function onSubmit(event) {
-        event.preventDefault();
+  async function onSubmit(event) {
+    event.preventDefault();
 
-        const formData = new FormData(event.target);
-        const username = formData.get('username');
-        const email = formData.get('email');
-        const password = formData.get('password');
-        const repass = formData.get('repass');
+    const formData = new FormData(event.target);
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const repass = formData.get("repass");
 
-
-        if (!username || !password || !email || !repass) {
-            return ctx.render(registerTemplate(onSubmit, true));
-        }
-
-        if (password != repass) {
-            return ctx.render(registerTemplate(onSubmit, false, true));
-
-        }
-
-        await register(username, email, password);
-        event.target.reset();
-        ctx.setUserNav();
-        ctx.page.redirect('/allCars');
+    if (!username || !password || !email || !repass) {
+      return ctx.render(registerTemplate(onSubmit, true));
     }
+
+    if (password != repass) {
+      return ctx.render(registerTemplate(onSubmit, false, true));
+    }
+
+    await register(username, email, password);
+    event.target.reset();
+    ctx.setUserNav();
+    ctx.page.redirect("/");
+  }
 }
