054d272b584445f428ea6c8da3c1024702365c1d
Hristo Atanasov <hristo_atanasov87@abv.bg>
2022-07-07 16:21:23 +0300

    Add files via upload

--- src/api/api.js
+++ src/api/api.js
@@ -1,91 +1,95 @@
 export const settings = {
-    host: ''
+  host: "",
 };
 
 async function request(url, options) {
-    try {
-        const response = await fetch(url, options);
+  try {
+    const response = await fetch(url, options);
 
-        if (response.ok === false) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-        try {
-            return await response.json();
-        } catch (err) {
-            return response;
-        }
-    } catch (error) {
-        alert(error.message);
-        throw error;
+    if (response.ok === false) {
+      const error = await response.json();
+      throw new Error(error.message);
     }
-
+    try {
+      return await response.json();
+    } catch (err) {
+      return response;
+    }
+  } catch (error) {
+    alert(error.message);
+    console.log(error);
+    throw error;
+  }
 }
 
-function createOptions(method = 'get', data) {
-    const result = {
-        method,
-        headers: {
-            'X-Parse-Application-Id': 'O7mrq94BjqFHm6z7RADiLr1keV7gi3utACxVY7s1',
-            'X-Parse-REST-API-Key': 'yntCke3Rfa9Fs6E6vuCFGAUma8FImi3y86vEf0KO'
-        }
-    }
+function createOptions(method = "get", data) {
+  const result = {
+    method,
+    headers: {
+      "X-Parse-Application-Id": "BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f",
+      "X-Parse-REST-API-Key": "swrFFIXJlFudtF3HkZPtfybDFRTmS7sPwvGUzQ9w",
+    },
+  };
 
-    if (data) {
-        result.headers['Content-type'] = 'application/json';
-        result.body = JSON.stringify(data);
-    }
+  if (data) {
+    result.headers["Content-type"] = "application/json";
+    result.body = JSON.stringify(data);
+  }
 
-    const token = sessionStorage.getItem('authToken');
-    if (token != null) {
-        result.headers['X-Parse-Session-Token'] = token;
-    }
+  const token = sessionStorage.getItem("authToken");
+  if (token != null) {
+    result.headers["X-Parse-Session-Token"] = token;
+  }
 
-    return result;
+  return result;
 }
 
 export async function get(url) {
-    return request(url, createOptions());
+  return request(url, createOptions());
 }
 
 export async function post(url, data) {
-    return request(url, createOptions('post', data));
+  return request(url, createOptions("post", data));
 }
 
 export async function put(url, data) {
-    return request(url, createOptions('put', data));
+  return request(url, createOptions("put", data));
 }
 
 export async function del(url) {
-    return request(url, createOptions('delete'));
+  return request(url, createOptions("delete"));
 }
 
 export async function login(username, password) {
-    const response = await post(settings.host + '/login', { username, password });
+  const response = await post(settings.host + "/login", { username, password });
 
-    sessionStorage.setItem('authToken', response.sessionToken);
-    sessionStorage.setItem('userId', response.objectId);
-    sessionStorage.setItem('username', username);
+  sessionStorage.setItem("authToken", response.sessionToken);
+  sessionStorage.setItem("userId", response.objectId);
+  sessionStorage.setItem("username", username);
 
-    return response;
+  return response;
 }
 
 export async function register(username, email, password) {
-    const response = await post(settings.host + '/users', { username, email, password });
+  const response = await post(settings.host + "/users", {
+    username,
+    email,
+    password,
+  });
 
-    sessionStorage.setItem('authToken', response.sessionToken);
-    sessionStorage.setItem('userId', response.objectId);
-    sessionStorage.setItem('username', username);
+  sessionStorage.setItem("authToken", response.sessionToken);
+  sessionStorage.setItem("userId", response.objectId);
+  sessionStorage.setItem("username", username);
 
-    return response;
+  return response;
 }
 
 export async function logout() {
-    const response = await post(settings.host + '/logout', {});
+  const response = await post(settings.host + "/logout", {});
 
-    sessionStorage.removeItem('authToken');
-    sessionStorage.removeItem('userId');
-    sessionStorage.removeItem('username');
+  sessionStorage.removeItem("authToken");
+  sessionStorage.removeItem("userId");
+  sessionStorage.removeItem("username");
 
-    return response;
+  return response;
 }
